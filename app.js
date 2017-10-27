8bf4ac3be817ae8a0f16c66781b46af7e6d89702
Ahmed T. Ali <ah.tajelsir@gmail.com>
2017-10-27 10:38:53 +0300

    refactor: prepare for our API

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 // Packages
-const next = require('next')
 const Koa = require('koa')
 const Router = require('koa-router')
+const Next = require('next')
 
 // Ours
+const api = require('./api')
 const { dev, port } = require('./config/env')
 
-const app = next({ dev })
+const app = Next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   const server = new Koa()
   const router = new Router()
 
-  // Routes
+  // API routes
+  router.use(api.routes())
+  router.use(api.allowedMethods())
+
+  // Other routes
   router.get('*', async ctx => {
     await handle(ctx.req, ctx.res)
     ctx.respond = false
