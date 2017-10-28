debec05fd41953760db30daf2f97228742074cef
Ahmed T. Ali <ah.tajelsir@gmail.com>
2017-10-28 13:34:22 +0300

    feat: initiate authorization flow when /login

--- config/env.js
+++ config/env.js
@@ -1,4 +1,5 @@
 module.exports = {
-  port: parseInt(process.env.PORT, 10) || 3000,
-  dev: process.env.NODE_ENV !== 'production'
+	client_id: process.env.AUTH0_CLIENT_ID,
+	dev: process.env.NODE_ENV !== 'production',
+	port: parseInt(process.env.PORT, 10) || 3000
 }
