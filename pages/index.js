f3c11296bae10e2c0fa7abc706bf1b600e3cd547
Ahmed T. Ali <ah.tajelsir@gmail.com>
2017-12-16 14:09:36 +0200

    feat: basic index page

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,13 @@
-export default () => <h1>Hello world!</h1>
+// Packages
+import Head from 'next/head'
+
+const IndexPage = () => [
+	<Head>
+		<title>Submit | A Prettier Interface For Submitting GitHub Issues</title>
+	</Head>,
+	<form action="/form" method="get">
+		<input name="repo" type="text" />
+	</form>
+]
+
+export default IndexPage
