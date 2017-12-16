e133d203c6dd36b129da7b0c3d52eb7d2e88761b
Ahmed T. Ali <ah.tajelsir@gmail.com>
2017-12-16 16:15:20 +0200

    feat: From API endpoint

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const Router = require('koa-router')
 
 // Ours
 const passport = require('../lib/auth')
+const github = require('../lib/github')
 
 // Globals
 const api = new Router()
@@ -37,6 +38,39 @@ api.get('/userinfo', async ctx => {
 	ctx.assert(ctx.isAuthenticated(), 401, 'Unauthorized!')
 	ctx.type = 'application/json'
 	ctx.body = JSON.stringify(ctx.state.user)
+})
+
+//=============================================================================
+// > Form endpoint
+//=============================================================================
+
+api.post('/form', async ctx => {
+	ctx.assert(ctx.isAuthenticated(), 401, 'Unauthorized!')
+	ctx.type = 'application/json'
+
+	// OAuth2 token
+	const { token } = ctx.state.user
+	const query = ctx.query.repo
+
+	const repo = await github.getRepo(ctx.state.user.token, query)
+
+	// The repo param either invalid or the repository isn't found
+	// Return 404
+	if (!repo) {
+		ctx.status = 404
+		return
+	}
+
+	// Extract necessary details
+	const { full_name, archived, default_branch, has_issues } = repo
+
+	// Will we be able to create a new issue?
+	const possible = has_issues && !archived
+
+	// Get repository Form
+	const form = await github.getForm(full_name, default_branch)
+
+	ctx.body = JSON.stringify({ full_name, possible, form })
 })
 
 module.exports = api
--- pages/form.js
+++ pages/form.js
@@ -1,8 +1,25 @@
 // Ours
 import { host } from '../config/client'
+import fetch from '../lib/fetch'
 
 class FormPage extends React.Component {
+	static async getInitialProps({ req }) {
+		const res = await fetch(req, req.url, { method: 'POST' })
+		if (res.ok) {
+			const details = await res.json()
+			return { ...details }
+		}
+		return {}
+	}
+
 	render() {
+		if (!this.props.possible) {
+			return (
+				<h1>
+					The repository is either archived or the issues are disabled, Sorry!
+				</h1>
+			)
+		}
 		return <h1>Nothing yet, Sorry!</h1>
 	}
 }
