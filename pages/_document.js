f6a8243ebf9af649e9e1dfdf09b2ef151b1a76d0
Ahmed T. Ali <ah.tajelsir@gmail.com>
2017-12-20 17:42:55 +0200

    fix meta attributes

--- pages/_document.js
+++ pages/_document.js
@@ -26,12 +26,12 @@ export default class MyDocument extends Document {
 		return (
 			<html lang="en">
 				<Head>
-					<meta charset="UTF-8" />
+					<meta charSet="UTF-8" />
 					<meta
 						name="viewport"
 						content="width=device-width, initial-scale=1.0"
 					/>
-					<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+					<meta httpEquiv="X-UA-Compatible" content="ie=edge" />
 					<title>
 						Submitter 🚀 | A Prettier UI for Submitting GitHub Issues
 					</title>
